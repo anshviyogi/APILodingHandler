ea32171bf02cf4735b60e577994736f74a56c97e
AnshViyogi <auni.viyogi@gmail.com>
2022-09-26 16:04:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React,{Suspense} from "react";
+import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import GetUser from './component/GetUser'
+import Home from './COMPONENTS/Admin/Home'
+import Login from './COMPONENTS/Login'
+import Sidebar from './COMPONENTS/Sidebar'
+import Users from './COMPONENTS/Admin/Users'
+import TaskList from './COMPONENTS/Admin/TaskList'
+import Tasks from './COMPONENTS/Admin/Tasks'
+import {useSelector} from 'react-redux'
 
-const Home = React.lazy(()=> import('./component/Home'))
+function App() {
+  // Use Selector - getting the value
+  const status = useSelector(state => state.user)
 
-function App(){
-return(
-  <>
-  <Router>
-    
-  <Suspense fallback={<div>Loading . . .</div>}>
-    <Routes>
+  console.log(status)
+  return (
+    <Router>
+      <Routes>
+        {/* Admin Home Routes */}
+        <Route path='/dashboard' element={<><Sidebar/><Home/></>}/>
+        <Route path='/dashboard/users' element={<><Sidebar/><Users/></>}/>
+        <Route path='/dashboard/tasklist' element={<><Sidebar/><TaskList/></>}/>
+        <Route path='/dashboard/tasks' element={<><Sidebar/><Tasks/></>}/>
 
-    <Route path='/' element={<Home/>}/>
-      <Route path='/showUsers' element={<><Home/><GetUser/></>}/>
-    </Routes>
-  </Suspense>
-  </Router>
-  </>
-)
+        {/* Login/Main Route */}
+        <Route exact path='/' element={<Login/>}/>
+      </Routes>
+    </Router>
+  )
 }
+
 export default App
--- src/component/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Loading() {
-  return (
-    <div>Loading Content . . . </div>
-  )
-}
-
-export default Loading
